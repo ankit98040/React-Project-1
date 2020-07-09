9b5eca288e343ad357dfb0c7178cf8728ff88988
Ankit <Pramanik.85849@gmail.com>
2020-07-09 11:03:18 +0530

    link tags
    link tags

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -7,6 +7,7 @@ const UsersItem = props => {
     return (
         <li className="user-item">
             <div className="user-item__content">
+                <Link>
                 <div className="user-item__image">
                     <Avatar image={props.image} alt={props.name} />
                 </div>
@@ -14,6 +15,7 @@ const UsersItem = props => {
                     <h2>{props.name}</h2>
                     <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
                 </div>
+                </Link>
             </div>
         </li>
     );
