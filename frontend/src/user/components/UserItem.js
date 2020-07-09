693744678141b002e65f20f8c36710351370b216
Ankit <Pramanik.85849@gmail.com>
2020-07-09 10:53:44 +0530

    changed image to avatar
    changd

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -7,7 +7,7 @@ const UsersItem = props => {
         <li className="user-item">
             <div className="user-item__content">
                 <div className="user-item__image">
-                    <img src={props.image} alt={props.name} />
+                    <Avatar image={props.image} alt={props.name} />
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
